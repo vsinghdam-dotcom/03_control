40f4d3ebae9e15e9b7bc0ba8f8e4a4e42a358aff
vsinghdam-dotcom <vsingh.dam@institutcampalans.net>
2026-09-24 16:16:04 +0200

    Segona Aplicacion

--- 03_control/MainWindow.xaml.cs
+++ 03_control/MainWindow.xaml.cs
@@ -23,6 +23,34 @@ namespace _03_control
         public MainWindow()
         {
             InitializeComponent();
+            Grid elMeuGrid = new Grid();
+            this.Content = elMeuGrid;
+
+            Button btn = new Button();
+            WrapPanel elMeuWrap = new WrapPanel();
+
+            TextBlock txt1 = new TextBlock();
+            txt1.Text = "Hola";
+            txt1.Foreground = Brushes.Red;
+            elMeuWrap.Children.Add(txt1);
+
+            TextBlock txt2 = new TextBlock();
+            txt2.Text = "Adeu";
+            txt2.Foreground = Brushes.Yellow;
+            elMeuWrap.Children.Add(txt2);
+
+            TextBlock txt3 = new TextBlock();
+            txt3.Text = "Andreu";
+            txt2.Foreground = Brushes.Green;
+            elMeuWrap.Children.Add(txt3);
+
+
+            btn.Width = 150;
+            btn.Height = 50;
+            btn.FontSize = 15;
+            btn.Content = elMeuWrap;
+
+            elMeuGrid.Children.Add(btn);
         }
     }
 }
